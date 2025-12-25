75e37e3fca1b0e66984004a2ca1e22b1b91b98e8
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-12-25 18:52:25 +0700

    Menambahkan fitur tampil detail product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,4 +14,8 @@ class ProductController extends Controller
 
     return view('products.index', compact('products'));
 }
+    public function show(Product $product)
+    {
+        return view('products.show', compact(('product')));
+    }
 }
